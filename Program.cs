7a1bb443f4abdb213352144ec97a33fca7f3e75e
Piotr Puszkarski <piotrpuszk@gmail.com>
2023-07-20 20:44:34 +0200

    Test

--- Program.cs
+++ Program.cs
@@ -31,5 +31,10 @@ namespace Test
         {
             System.Console.WriteLine("Client!!!");
         }
+
+        public void Test()
+        {
+            System.Console.WriteLine("Test");
+        }
     }
 }
