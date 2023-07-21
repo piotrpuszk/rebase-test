b22a6540070d1904a4b88d4eff9702c41ffdfd8f
Piotr Puszkarski <piotrpuszk@gmail.com>
2023-07-21 14:56:38 +0200

    exclamation mark added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace Test
     {
         public void Feature()
         {
-            System.Console.WriteLine("feature final");
+            System.Console.WriteLine("feature final!");
         }
 
         public void FeatureImprovement()
